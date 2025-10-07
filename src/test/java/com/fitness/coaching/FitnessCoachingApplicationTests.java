1f199c0816f1570adc01521d5a9bf0051524337c
udune <udune@naver.com>
2025-10-07 15:10:53 +0900

    init

---
+++ src/test/java/com/fitness/coaching/FitnessCoachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.coaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessCoachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
