b0c1db589f20682527b5e9c3f2e8bd49a948977e
udune <udune@naver.com>
2025-10-26 21:52:53 +0900

    chore: CorsCofig 수정

--- src/main/java/com/fitness/coaching/config/CorsConfig.java
+++ src/main/java/com/fitness/coaching/config/CorsConfig.java
@@ -8,7 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${cors.allowed-origins:http://localhost:3000,http://localhost:3001}")
+    @Value("${cors.allowed-origins:" +
+            "http://localhost:3000," +
+            "http://localhost:3001," +
+            "http://localhost:5173," +
+            "https://pt-coach.vercel.app}")
     private String[] allowedOrigins;
 
     @Override
